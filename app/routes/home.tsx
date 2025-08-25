6d1a1c07c22ffa9749b4d1f9cf85ca964665f5f1
Vamsi4466 <settyvamsikrishna83@gmail.com>
2025-08-25 19:44:38 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,29 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumify.ai" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your Applications & Resume Ratings</h1>
+        <h2>review your submissions and check AI_powered feedback.</h2>
+      </div>
+    </section>
+
+    <div className="resumes-section">
+      {resumes.map((resume: Resume) => (
+        <ResumeCard key={resume.id} resume={resume} />
+      ))}
+    </div>
+  </main>
 }
